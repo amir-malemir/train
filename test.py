4a3a6a57394e904e695f9cfee849936f747b9068
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-07-26 12:30:35 +0330

    puzzle task

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print('hello world')
+#  for puzzle task
+
+n, m = map(int, input().split())
+puzzle_input = list(map(int, input().split()))
+puzzle_input.sort()
+output = 0
+
+puzzle_num = [0] * m
+
+for i in range(m-n+1):
+    dif = puzzle_input[i+n-1] - puzzle_input[i]
+    if dif <= output:
+        output = dif
+    elif output == 0:
+        output = dif
+
+print(output)
