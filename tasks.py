6f7c4a84576082f6eb527178e8ebf3ab9c6d7475
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-07-28 02:00:12 +0330

    finaly boy adn girl task

--- tasks.py
+++ tasks.py
@@ -20,3 +20,10 @@ print(output)
 
 # boy and girl in chatroom
 
+n = set(input())
+m = len(n)
+
+if m % 2 == 0:
+    print('CHAT WITH HER!')
+else:
+    print('IGNORE HIM!')
--- test.py
+++
@@ -1,7 +0,0 @@
-n = set(input())
-m = len(n)
-
-if m % 2 == 0:
-    print('CHAT WITH HER!')
-else:
-    print('IGNORE HIM!')
