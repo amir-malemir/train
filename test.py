d4076d78517f8db672ae61c3b31fab7fdeacda87
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-07-28 20:31:07 +0330

    fix proble of if and status of dragon wars

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 s, n = map(int, input().split())
 dragon_lists = []
 
-status = 0
+status = False
 
 for i in range(n):
     dragon_attack, dragon_bonus = map(int, input().split())
@@ -10,13 +10,14 @@ dragon_lists.sort()
 for dragon in dragon_lists:
     attk = dragon[0]
     bonus = dragon[1]
-    if s >= attk:
+    if s > attk:
         s = s + bonus
-        status = 1
+        status = True
         
     else:
-        status = 0
-
+        status = False
+        break
+    
 if status == 1:
     print('YES')
 else:
