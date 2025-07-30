d5004ea5465700e014f42f037cd69f053af88e7c
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-07-30 20:25:24 +0330

    replace No to NO :)

--- test.py
+++ test.py
@@ -6,9 +6,7 @@ string_convert = input()
 mix_input = mehman + mizban
 mix_input = "".join(sorted(mix_input))
 diff_string = "".join(sorted(string_convert))
-print(mix_input)
-print(diff_string)
 if mix_input == diff_string:
     print('YES')
 else:
-    print('No')
+    print('NO')
