b79f919f919911e99c8e6fa1024ea52fab9f0734
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-07-30 17:26:06 +0330

    start solve Amusing Joke task

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-n, l = map(int, input().split())
-fanos_l = list(map(int, input().split()))
-middle = 0
+mehman = input()
+mizban = input()
+string_convert = input()
 
-fanos_l.sort()
-
-if n > 1:    
-    for i in range(n-1):
-        calcu = fanos_l[i+1] - fanos_l[i]
-        if calcu > middle:
-            middle = calcu
-middle = middle/ 2
-first_l = fanos_l[0] - 0
-last_l = l - fanos_l[n-1]
-output = max(first_l, last_l, middle)
-print(output)
+if mehman and mizban in string_convert:
+    print('YES')
+print('NO')
