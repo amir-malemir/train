297859a21f8b349755e570ebe8031ca8111c92de
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-07-30 17:46:44 +0330

    create for for check len str

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ mehman = input()
 mizban = input()
 string_convert = input()
 
-if mehman and mizban in string_convert:
-    print('YES')
+for i in range(1 , len(mehman)):
+    check_mehman = mehman[i:]
+    check_mizban = mizban[i:]
+    if check_mehman and check_mizban in string_convert:
+        print('YES')
 print('NO')
