ce69ca633cf8e9e1b6c388363ccf02214c0cc033
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-07-31 22:11:07 +0330

    some idea for fix problem

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 n = int(input())
 month = list(map(int , input().split()))
 counter = 0
+sum_number = 0
 month.sort(reverse=True)
+print(month)
 
 for i in range(11):
     counter = counter + 1
-    sum_number = month[i+1] + month[i]
+    sum_number = sum_number + month[i]
     print(sum_number)
     if sum_number >= n:
         print(counter)
+        break
     else:
         print('no')
         
