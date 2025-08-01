5610a0923a30891c5f3f1ea876574d29be36220b
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-01 22:21:19 +0330

    solve Business trip task of codeforces

--- tasks.py
+++ tasks.py
@@ -111,3 +111,24 @@ if mix_input == diff_string:
     print('YES')
 else:
     print('NO')
+
+# Business trip
+
+
+n = int(input())
+month = list(map(int , input().split()))
+counter = 0
+sum_number = 0
+month.sort(reverse=True)
+for i in range(11):
+    counter = counter + 1
+    sum_number = sum_number + month[i]
+    if sum_number >= n:
+        print(counter)
+        break
+    elif n == 0:
+        print(1)
+        break
+    elif i == 11 or counter == 0:
+        print('-1')
+        
