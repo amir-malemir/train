fbd3ffd6e88b27faf34413ec0d28e551eab54a01
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-02 23:29:44 +0330

    fix bug for test 3 of codeforces

--- test.py
+++ test.py
@@ -2,16 +2,16 @@ n = int(input())
 people = []
 in_people = 0
 out_people = 0
+calc = 0
 max_num = 0
 
 for i in range(n):
     outside, inside = map(int, input().split())
     people.append((outside, inside))
 for peo in people:
+    calc = calc + peo[1] - peo[0] 
     in_people = peo[1]
     out_people = peo[0]
-    calc = max_num + peo[1] - peo[0] 
-    print(calc)
     if calc > max_num:
         max_num = calc
 
