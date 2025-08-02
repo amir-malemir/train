a312b0794c9173399c4c3b19afdca720cdcd00b8
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-03 00:14:45 +0330

    fix for int input less than 26 alphabet

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import string
 all_alphabet = string.ascii_lowercase
-m = input()
+m = int(input())
 n = list(input().lower())
-n.sort()
-input_user = set(n)
-output_char = "".join(sorted(input_user)).lower()
-if output_char in all_alphabet:
-    print('YES')
+if m >= 26:
+    n.sort()
+    input_user = set(n)
+    output_char = "".join(sorted(input_user)).lower()
+    if output_char in all_alphabet:
+        print('YES')
+    else:
+        print('NO')
 else:
     print('NO')
