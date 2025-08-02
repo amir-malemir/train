6915e086633afd3bcbef0e02cf787f3824ef1e5a
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-02 23:30:23 +0330

    solve Tram task of CodeForces

--- tasks.py
+++ tasks.py
@@ -135,3 +135,23 @@ if flag == False:
     print('-1')
 
 # Tram
+
+n = int(input())
+people = []
+in_people = 0
+out_people = 0
+calc = 0
+max_num = 0
+
+for i in range(n):
+    outside, inside = map(int, input().split())
+    people.append((outside, inside))
+for peo in people:
+    calc = calc + peo[1] - peo[0] 
+    in_people = peo[1]
+    out_people = peo[0]
+    if calc > max_num:
+        max_num = calc
+
+
+print(max_num)
