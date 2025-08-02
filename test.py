b99305d87cf284391e85e0ca1fe65a92e3dfaee2
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-03 00:09:54 +0330

    fix output for answer

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ n = list(input().lower())
 n.sort()
 input_user = set(n)
 output_char = "".join(sorted(input_user)).lower()
-print(output_char)
 if output_char in all_alphabet:
     print('YES')
 else:
