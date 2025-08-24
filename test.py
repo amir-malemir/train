12b97c9ecbc87c3bbc817f02453e82d9dc4d280e
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-24 14:03:40 +0330

    try to follow bainry search

--- test.py
+++ test.py
@@ -3,3 +3,11 @@ worm_gr = input()
 m = int(input())
 which_worm = input()
 
+low = 0
+high = 0
+while True:
+    for i in range(m):
+        if which_worm[[i]] <= low:
+            print('low')
+        elif which_worm > low:
+            print('high')
