49f00541ed63b06a016f33da0b49bd2818b71ede
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-24 14:08:39 +0330

    create list of input for bainary search

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 n = int(input())
-worm_gr = input()
+worm_gr = list(map(int, input().split()))
 m = int(input())
-which_worm = input()
+which_worm = list(map(int, input().split()))
 
+for i in worm_gr:
+    sum_list += i
+print(sum_list)
 low = 0
 high = 0
-while True:
-    for i in range(m):
-        if which_worm[[i]] <= low:
-            print('low')
-        elif which_worm > low:
-            print('high')
+
+# while True:
+#     for i in range(m):
+#         if which_worm[[i]] <= low:
+#             print('low')
+#         elif which_worm > low:
+#             print('high')
+#     break
