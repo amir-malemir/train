6fc9ac75cc9f3197953747d585521c1d7a8e9fda
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-27 12:32:56 +0330

    finished worm task from codeforces worm-b-474

--- test.py
+++ test.py
@@ -2,19 +2,24 @@ n = int(input())
 worm_gr = list(map(int, input().split()))
 m = int(input())
 which_worm = list(map(int, input().split()))
-prefix_sums = []
+sums = []
 current_sum = 0
 for i in worm_gr:
     current_sum += i
-    prefix_sums.append(current_sum)
-print(prefix_sums)
-low = 0
-high = 0
+    sums.append(current_sum)
+print(sums)
+for worm_num in which_worm:
+    low = 0
+    high = n - 1
+    answer = 0
+    while low <= high:
+        mid = (low + high) // 2
+        if sums[mid] >= worm_num:
+            answer = mid
+            high = mid - 1
+        else:
+            low = mid + 1
 
-# while True:
-#     for i in range(m):
-#         if which_worm[[i]] <= low:
-#             print('low')
-#         elif which_worm > low:
-#             print('high')
-#     break
+    print(answer + 1)
+
+        
