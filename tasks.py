3bf2bcf24460ca90b1c4b40fce6921599212c8cf
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-27 13:10:28 +0330

    Solved Worms 474B

--- tasks.py
+++ tasks.py
@@ -171,3 +171,30 @@ if len(set(n)) >= 26:
         print('NO')
 else:
     print('NO')
+
+
+# Worms 474B
+
+n = int(input())
+worm_gr = list(map(int, input().split()))
+m = int(input())
+which_worm = list(map(int, input().split()))
+sums = []
+current_sum = 0
+for i in worm_gr:
+    current_sum += i
+    sums.append(current_sum)
+for worm_num in which_worm:
+    low = 0
+    high = n - 1
+    answer = 0
+    while low <= high:
+        mid = (low + high) // 2
+        if sums[mid] >= worm_num:
+            answer = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    print(answer + 1)
+ 
