2f0dcad5357771ce4fe61b8bee7309fed498b00e
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-31 18:44:51 +0330

    some way for fix problem

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 n, t = map(int, input().split())
-        
+spend_time = list(map(int, input().split()))
+read_book = 0
+counter = 0
+i = 0
+
+for i in range(n):
+    sum_books = 0
+    
+    counter += 1
+    print(' for clear !!!!!!!!')
+    for j in spend_time:
+        print(spend_time[counter])
+        # print(f'counter {counter}')
+        # print(f'j {j}')
+        # sum_books += spend_time[counter-1]
+        # print (f'in secound for : {sum_books}')
