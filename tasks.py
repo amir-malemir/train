da3bb79d827193779a4319e1d2203822160f4aa8
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-31 21:39:48 +0330

    finally code for Books 279/B codeforces

--- tasks.py
+++ tasks.py
@@ -198,3 +198,27 @@ for worm_num in which_worm:
 
     print(answer + 1)
  
+
+
+# Books 279/B
+
+#     sum_books = 0
+#     time_for_read = 0
+#     counter = 0
+#     print(' first for !!!!!!!!')
+#     for j in spend_time:
+#         # print(f'j--> {j}')
+
+#         time_for_read += j
+#         print(f'time for read--> {time_for_read}')
+#         if time_for_read <= t:
+#             sum_books += 1
+#             print(f'sumbook--> {sum_books}')
+#         else:
+#             time_for_read -= spend_time[0]
+#             left += 1
+#             break
+
+#     if sum_books > read_book:
+#         read_book = sum_books
+# print(f'last answer : {read_book}')
--- test.py
+++ test.py
@@ -13,23 +13,3 @@ for i in range(n):
 
     read_book = max(read_book, i - next_number + 1)
 print(read_book)
-#     sum_books = 0
-#     time_for_read = 0
-#     counter = 0
-#     print(' first for !!!!!!!!')
-#     for j in spend_time:
-#         # print(f'j--> {j}')
-
-#         time_for_read += j
-#         print(f'time for read--> {time_for_read}')
-#         if time_for_read <= t:
-#             sum_books += 1
-#             print(f'sumbook--> {sum_books}')
-#         else:
-#             time_for_read -= spend_time[0]
-#             left += 1
-#             break
-
-#     if sum_books > read_book:
-#         read_book = sum_books
-# print(f'last answer : {read_book}')
