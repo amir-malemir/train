bff11a4b01fccf44e27a5de83c8634a3821f416d
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-31 21:37:47 +0330

    some help and advice

--- test.py
+++ test.py
@@ -1,27 +1,35 @@
 n, t = map(int, input().split())
 spend_time = list(map(int, input().split()))
 read_book = 0
-counter = 0
-left = 0
+next_number = 0
+time_for_read= 0
+for i in range(n):
+    time_for_read += spend_time[i]
 
-for i in range(n-1):
-    sum_books = 0
-    time_for_read = 0
-    counter = 0
-    print(' first for !!!!!!!!')
-    for j in spend_time:
-        # print(f'j--> {j}')
 
-        time_for_read += j
-        print(f'time for read--> {time_for_read}')
-        if time_for_read <= t:
-            sum_books += 1
-            print(f'sumbook--> {sum_books}')
-        else:
-            time_for_read -= spend_time[0]
-            left += 1
-            break
+    while time_for_read > t:
+        time_for_read -= spend_time[next_number]
+        next_number += 1
 
-    if sum_books > read_book:
-        read_book = sum_books
-print(f'last answer : {read_book}')
+    read_book = max(read_book, i - next_number + 1)
+print(read_book)
+#     sum_books = 0
+#     time_for_read = 0
+#     counter = 0
+#     print(' first for !!!!!!!!')
+#     for j in spend_time:
+#         # print(f'j--> {j}')
+
+#         time_for_read += j
+#         print(f'time for read--> {time_for_read}')
+#         if time_for_read <= t:
+#             sum_books += 1
+#             print(f'sumbook--> {sum_books}')
+#         else:
+#             time_for_read -= spend_time[0]
+#             left += 1
+#             break
+
+#     if sum_books > read_book:
+#         read_book = sum_books
+# print(f'last answer : {read_book}')
