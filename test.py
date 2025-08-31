88cd5d23df7ce601b5b500394212af04d8398816
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-08-31 19:08:14 +0330

    fix problems

--- test.py
+++ test.py
@@ -7,16 +7,19 @@ i = 0
 for i in range(n-1):
     sum_books = 0
     time_for_read = 0
-    
+    counter = 0
     print(' first for !!!!!!!!')
     for j in spend_time:
+        print(f'j--> {j}')
+
         time_for_read += j
         print(f'time for read--> {time_for_read}')
         if time_for_read <= t:
             sum_books += 1
-            print(f'time for read--> {sum_books}')
+            print(f'sumbook--> {sum_books}')
         else:
             break
 
     if sum_books > read_book:
         read_book = sum_books
+print(f'last answer : {read_book}')
