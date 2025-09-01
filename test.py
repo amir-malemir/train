01748965848c50406177af77084a3457c7715333
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-09-01 13:48:08 +0330

    clear list for new round

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
 counter = 0 
-list_of_numbers = []
 for i in range(m):
     l = int(input())
     index_number = l - 1
+    list_of_numbers = []
+    
     for j in range(index_number,n):
-        list_of_numbers.append(j)
+        list_of_numbers.append(index_number[j])
 
     output_numbers = set(list_of_numbers)   
     print(len(output_numbers))
