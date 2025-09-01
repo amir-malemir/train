1da3609bf185626b3e291624140acbc114351769
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-09-01 13:35:42 +0330

    some idea for fix problem

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
-l = int(input())
 counter = 0 
 
 for i in range(n):
+    l = int(input())
     counter += 1
     for j in range(l,n):
         print(numbers[j])
+        
