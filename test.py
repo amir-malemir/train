1b13e311c8e9eb112a4dc3120a13ca1b41cabf18
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-09-02 02:52:58 +0330

    solve problem of codefroces sereja and suffixes

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
+#  B. Sereja and Suffixes
+
 n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
 list_of_questions = []
 counter = 0 
 for i in range(m):
     l = int(input())
-    list_of_questions.append[l]
+    list_of_questions.append(l)
 
 for x in range(l):
-    index_number = x - 1
+    index_number = x
     
     list_of_numbers = []
     
     for j in range(index_number,n):
         list_of_numbers.append(numbers[j])
 
-output_numbers = set(list_of_numbers)   
-print(len(output_numbers))
+    output_numbers = set(list_of_numbers)   
+    print(len(output_numbers))
