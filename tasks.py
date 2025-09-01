2584dab6344274225c6580e85856a18ca79799f8
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-09-02 03:16:23 +0330

    save code

--- tasks.py
+++ tasks.py
@@ -243,4 +243,23 @@ print(books_read)
 
 
 
+#  B. Sereja and Suffixes
 
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+list_of_questions = []
+counter = 0 
+while counter < m:
+    l = int(input())
+    list_of_questions.append(l)
+    counter += 1
+
+for x in range(m):
+    index_number = list_of_questions[x] - 1
+    
+    list_of_numbers = []
+    
+    for j in range(index_number,n):
+        list_of_numbers.append(numbers[j])
+    output_numbers = set(list_of_numbers)   
+    print(len(output_numbers))
