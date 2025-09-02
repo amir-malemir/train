64b2d6cbaf72bdbd2f1b113b6cfab95198c71291
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-09-02 13:33:52 +0330

    clear comments

--- test.py
+++ test.py
@@ -6,28 +6,17 @@ list_of_questions = []
 list_numbers = set()
 results = [0] * n
 counter = 0
-index_number = 0
-x = 0
+
+
 while counter < m:
     l = int(input())
     list_of_questions.append(l)
     counter += 1
 
-for i in range(n-1, -1):
+for i in range(n-1, -1, -1):
     list_numbers.add(numbers[i])
     counter = len(list_numbers)
     results[i] = counter
-print(list_numbers)
 
 for l in list_of_questions:
     print(results[l - 1])
-
-
-# for j in range(index_number, n):
-#     list_of_numbers = []
-#     if x < m:
-#         index_number = list_of_questions[x] - 1
-#         x += 1
-#     list_of_numbers.append(numbers[j])
-# output_numbers = set(list_of_numbers)   
-# print(len(output_numbers))
