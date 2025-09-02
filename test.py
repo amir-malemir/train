7a58e3a91763e53506ce573ee76fb3fba8a36b7d
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-09-02 12:09:05 +0330

    with some tip solve Sereja and Suffixes  task

--- test.py
+++ test.py
@@ -3,18 +3,31 @@
 n, m = map(int, input().split())
 numbers = list(map(int, input().split()))
 list_of_questions = []
-counter = 0 
+list_numbers = set()
+results = [0] * n
+counter = 0
+index_number = 0
+x = 0
 while counter < m:
     l = int(input())
     list_of_questions.append(l)
     counter += 1
 
-for x in range(m):
-    index_number = list_of_questions[x] - 1
-    
-    list_of_numbers = []
-    
-    for j in range(index_number,n):
-        list_of_numbers.append(numbers[j])
-    output_numbers = set(list_of_numbers)   
-    print(len(output_numbers))
+for i in range(n-1, -1):
+    list_numbers.add(numbers[i])
+    counter = len(list_numbers)
+    results[i] = counter
+print(list_numbers)
+
+for l in list_of_questions:
+    print(results[l - 1])
+
+
+# for j in range(index_number, n):
+#     list_of_numbers = []
+#     if x < m:
+#         index_number = list_of_questions[x] - 1
+#         x += 1
+#     list_of_numbers.append(numbers[j])
+# output_numbers = set(list_of_numbers)   
+# print(len(output_numbers))
