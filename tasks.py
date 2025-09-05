c8040c5dab882748e47ed29077c5bda2b1a56ee1
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-09-05 23:09:49 +0330

    finally #  B. Sereja and Suffixes - best performance

--- tasks.py
+++ tasks.py
@@ -263,3 +263,27 @@ for x in range(m):
         list_of_numbers.append(numbers[j])
     output_numbers = set(list_of_numbers)   
     print(len(output_numbers))
+
+
+#  B. Sereja and Suffixes - best performance
+
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+list_of_questions = []
+list_numbers = set()
+results = [0] * n
+counter = 0
+
+
+while counter < m:
+    l = int(input())
+    list_of_questions.append(l)
+    counter += 1
+
+for i in range(n-1, -1, -1):
+    list_numbers.add(numbers[i])
+    counter = len(list_numbers)
+    results[i] = counter
+
+for l in list_of_questions:
+    print(results[l - 1])
