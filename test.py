41c5f28de9bc3c68f7355dca033d8cf39428bf85
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-10-04 22:51:30 +0330

    try to compactifying list

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
 # C. Qpwoeirut And The City
-n = int(input())
-m = int(input())
-i = 0
+# n = int(input())
+# m = int(input())
+# i = 0
 
-while i < n:
-    print(m)
+# while i < n:
+#     print(m)
 
-    # test
+# L = 0 
+# read_ptr = L
+# write_ptr = 1
+# L = 1
+
+# if L == 0:
+#     print('Error - empty list')
+
+#     while read_ptr != 0:
+#         next_read_ptr = read_ptr + 1
+
+    
