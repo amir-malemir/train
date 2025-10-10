169e3cceadf60b506cc081e7e81a1545853e7f33
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-10-10 14:25:40 +0330

    move codes to fun.py

--- fun.py
+++ fun.py
@@ -36,3 +36,35 @@ bmi = get_bmi(number1, number2)
 bmi = "{:.2f}".format(bmi)
 print(bmi)
 bmi_ranges(float(bmi))
+
+# L = 0 
+# read_ptr = L
+# write_ptr = 1
+# L = 1
+
+# if L == 0:
+#     print('Error - empty list')
+
+#     while read_ptr != 0:
+#         next_read_ptr = read_ptr + 1
+
+
+
+
+# students = []
+# while True:
+#     name = input('name? ')
+#     age = input('age? ')
+#     while not age.isdigit():
+#         age = input('age? ')
+#     age = int(age)
+#     students.append({'name': name, 'age': age})
+#     resume = input('add? (n/y) ')
+#     if resume.lower() == 'n':
+#         break
+# print('*' * 10)
+
+# for stu in students:
+#     print(stu['name'], stu['age'])
+
+# print('*' * 10)
--- test.py
+++ test.py
@@ -6,34 +6,3 @@
 # while i < n:
 #     print(m)
 
-# L = 0 
-# read_ptr = L
-# write_ptr = 1
-# L = 1
-
-# if L == 0:
-#     print('Error - empty list')
-
-#     while read_ptr != 0:
-#         next_read_ptr = read_ptr + 1
-
-
-
-
-# students = []
-# while True:
-#     name = input('name? ')
-#     age = input('age? ')
-#     while not age.isdigit():
-#         age = input('age? ')
-#     age = int(age)
-#     students.append({'name': name, 'age': age})
-#     resume = input('add? (n/y) ')
-#     if resume.lower() == 'n':
-#         break
-# print('*' * 10)
-
-# for stu in students:
-#     print(stu['name'], stu['age'])
-
-# print('*' * 10)
