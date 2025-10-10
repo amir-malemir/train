e7468d6f71e956450333ce9e342ddc0f76c7a732
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-10-10 14:41:08 +0330

    fix lower and space for input and availabe cars

--- test.py
+++ test.py
@@ -6,7 +6,23 @@
 # while i < n:
 #     print(m)
 
-def is_car_available(cars):
-    print(cars)
+def is_car_available(year:int , *cars):
+    available_cars = {
+        2025: ['BMW', 'audi  '],
+        2024: ['mercedes', 'BMW', 'jeep  ']
+    }
+    models = available_cars.get(year)
+    print(models)
+    models = [model.strip().lower() for model in models]
+    print(models)
+    
+    if not models:
+        print( 'sry, we dont have car for this year' )
+        return
+    for car.strip().lower() in cars:
+        if car in models:
+            print(f'{car.title()} is available')
+        else:
+            print(f'{car.title()} is not available')
 
-is_car_available('bmw')
+is_car_available(2024,'BMW', 'volvo  ')
