6e65347049feb78ec036ddcedb260289a3d8c46e
Amir Malemir <165894138+amir-malemir@users.noreply.github.com>
2025-10-10 14:49:05 +0330

    move is_car_available to fun.py

--- fun.py
+++ fun.py
@@ -68,3 +68,25 @@ bmi_ranges(float(bmi))
 #     print(stu['name'], stu['age'])
 
 # print('*' * 10)
+
+
+def is_car_available(year:int , *cars):
+    available_cars = {
+        2025: ['BMW', 'audi  '],
+        2024: ['mercedes', 'BMW', 'jeep  ']
+    }
+    models = available_cars.get(year)
+    print(models)
+    models = [model.strip().lower() for model in models]
+    print(models)
+    
+    if not models:
+        print( 'sry, we dont have car for this year' )
+        return
+    for car in cars:
+        if car.strip().lower() in models:
+            print(f'{car.title()} is available')
+        else:
+            print(f'{car.title()} is not available')
+
+is_car_available(2024,'BMW', 'volvo  ')
